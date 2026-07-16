7965884bf9494b43998d567264ea1c4e48bb0ce4
Abdullah Al Numan <al.32noman@gmail.com>
2026-07-16 20:58:17 +0600

    did nothing (#2)

---
+++ src/main/java/cse213finalproject/_1_librarymanagement_2431986_2220590_2411815_2411833/LoginViewController.java
@@ -0,0 +1,20 @@
+package cse213finalproject._1_librarymanagement_2431986_2220590_2411815_2411833;
+
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class LoginViewController
+{
+    @javafx.fxml.FXML
+    private TextField enterUserId;
+    @javafx.fxml.FXML
+    private Button loginButton;
+    @javafx.fxml.FXML
+    private PasswordField enterPassword;
+    @javafx.fxml.FXML
+    private TextField enterUserName;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }}
