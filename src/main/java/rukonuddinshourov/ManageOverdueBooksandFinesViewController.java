5020a0fd22ec448cba63c06791ec76d0623df506
RukonUddinShourov6t9 <2411833@iub.edu.bd>
2026-07-26 01:02:43 +0600

    fxml by shourov

--- src/main/java/rukonuddinshourov/ManageOverdueBooksandFinesViewController.java
+++ src/main/java/rukonuddinshourov/ManageOverdueBooksandFinesViewController.java
@@ -1,7 +1,55 @@
 package rukonuddinshourov;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
 public class ManageOverdueBooksandFinesViewController
 {
     @javafx.fxml.FXML
+    private TextField enterPaymentInformation;
+    @javafx.fxml.FXML
+    private TextField enterMemberId;
+    @javafx.fxml.FXML
+    private TableColumn bookReturnDateTableView1;
+    @javafx.fxml.FXML
+    private TableColumn bookReturnDateTableView;
+    @javafx.fxml.FXML
+    private Label showTotalFine;
+    @javafx.fxml.FXML
+    private TextField overdueDays;
+    @javafx.fxml.FXML
+    private TableColumn borrowedBookTableView;
+    @javafx.fxml.FXML
+    private TableView fineCollectionTableView;
+    @javafx.fxml.FXML
+    private TextField finePerDay;
+    @javafx.fxml.FXML
+    private TextField paymentStatus;
+
+    @javafx.fxml.FXML
     public void initialize() {
-    }}
+    }
+
+    @javafx.fxml.FXML
+    public void payFineNowButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void notificationSendToMember(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void searchMemberButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void calculateFineButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void generateFineReceiptButton(ActionEvent actionEvent) {
+    }
+}
---
+++ src/main/java/rukonuddinshourov/UpdateLibraryCatalogViewController.java
@@ -0,0 +1,48 @@
+package rukonuddinshourov;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
+
+public class UpdateLibraryCatalogViewController {
+    @javafx.fxml.FXML
+    private ComboBox Status;
+    @javafx.fxml.FXML
+    private TextField ISBN;
+    @javafx.fxml.FXML
+    private TextField shelfLocation;
+    @javafx.fxml.FXML
+    private TextField Title;
+    @javafx.fxml.FXML
+    private TextField Author;
+    @javafx.fxml.FXML
+    private TextField Quantity;
+    @javafx.fxml.FXML
+    private ComboBox category;
+    @javafx.fxml.FXML
+    private TextField bookId;
+
+    @javafx.fxml.FXML
+    public void confirmUpdateButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void refreshButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void Searchbutton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void updateButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void RemoveButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void ConfirmUpdateButton(ActionEvent actionEvent) {
+    }
+}
