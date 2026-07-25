28db2f9cf035025eac72f0755b20a9ced4b48fb6
RukonUddinShourov6t9 <2411833@iub.edu.bd>
2026-07-26 03:08:58 +0600

    fxml by shourov

---
+++ src/main/java/rukonuddinshourov/CreateNewAnnouncementViewController.java
@@ -0,0 +1,47 @@
+package rukonuddinshourov;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.*;
+
+public class CreateNewAnnouncementViewController {
+    @javafx.fxml.FXML
+    private DatePicker StartingDate;
+    @javafx.fxml.FXML
+    private DatePicker EndingDate;
+    @javafx.fxml.FXML
+    private ComboBox Category;
+    @javafx.fxml.FXML
+    private TextField attachment;
+    @javafx.fxml.FXML
+    private Label createNewAnnouncement;
+    @javafx.fxml.FXML
+    private TextArea optionalAttachmentView;
+    @javafx.fxml.FXML
+    private TextArea AnnouncementTitle;
+    @javafx.fxml.FXML
+    private ComboBox TargetAudiance;
+
+    @javafx.fxml.FXML
+    public void cancelButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void previewButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void publishButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void browseButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void resetButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void saveDraftButton(ActionEvent actionEvent) {
+    }
+}
