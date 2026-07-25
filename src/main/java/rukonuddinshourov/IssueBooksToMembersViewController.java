b76247d3027a99e5aee9937a33b97f1098d08a78
RukonUddinShourov6t9 <2411833@iub.edu.bd>
2026-07-25 15:57:29 +0600

    worked on fxmls(shourov)

---
+++ src/main/java/rukonuddinshourov/IssueBooksToMembersViewController.java
@@ -0,0 +1,37 @@
+package rukonuddinshourov;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextField;
+
+public class IssueBooksToMembersViewController {
+    @javafx.fxml.FXML
+    private Button confirmBookIssue;
+    @javafx.fxml.FXML
+    private Button checkBookAvailablity;
+    @javafx.fxml.FXML
+    private Button updateIssued;
+    @javafx.fxml.FXML
+    private TextField isbnTitle;
+    @javafx.fxml.FXML
+    private TextField memberName;
+    @javafx.fxml.FXML
+    private DatePicker issueDate;
+    @javafx.fxml.FXML
+    private TextField searchBook;
+    @javafx.fxml.FXML
+    private TextField memberId;
+
+    @javafx.fxml.FXML
+    public void SearchBookAvaiablity(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void UpdateToIssue(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void ConfirmIssueBook(ActionEvent actionEvent) {
+    }
+}
---
+++ src/main/java/rukonuddinshourov/Librarian.java
@@ -0,0 +1,6 @@
+package rukonuddinshourov;
+
+import user.Employee;
+
+public class Librarian extends Employee {
+}
