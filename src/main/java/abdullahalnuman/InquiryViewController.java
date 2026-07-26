5568d8e6f758bb073f4428b76fc77c59b89d1b18
numan6t9 <al.32noman@gmail.com>
2026-07-26 22:51:46 +0600

    completed fxmls

---
+++ src/main/java/abdullahalnuman/InquiryViewController.java
@@ -0,0 +1,23 @@
+package abdullahalnuman;
+
+public class InquiryViewController
+{
+    @javafx.fxml.FXML
+    private TextField inquiryDetails;
+    @javafx.fxml.FXML
+    private Label showMemberOrVisitorStatus;
+    @javafx.fxml.FXML
+    private TextField enterMemberIdorVisitorPass;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void saveInquiryButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void searchMemberOrVisitorButton(ActionEvent actionEvent) {
+    }
+}
---
+++ src/main/java/abdullahalnuman/UpdateMemberInformationView.java
@@ -0,0 +1,35 @@
+package abdullahalnuman;
+
+public class UpdateMemberInformationView
+{
+    @javafx.fxml.FXML
+    private Label showMemberId;
+    @javafx.fxml.FXML
+    private TextField enterMemberId;
+    @javafx.fxml.FXML
+    private Label showMemberName;
+    @javafx.fxml.FXML
+    private TextField updateEmail;
+    @javafx.fxml.FXML
+    private TextField enterMemberName;
+    @javafx.fxml.FXML
+    private TextField updateAddress;
+    @javafx.fxml.FXML
+    private Label showEmailAddress;
+    @javafx.fxml.FXML
+    private Label showAddress;
+    @javafx.fxml.FXML
+    private TextField updateName;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void updateMemberAndInformationAndSaveButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void searchMemberButton(ActionEvent actionEvent) {
+    }
+}
